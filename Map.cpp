be49573d25b9e18ba6b781a58fe6470bc013cf82
Porple <83104227+PorpleFish@users.noreply.github.com>
2025-12-01 18:43:04 +1100

    Fixxed tile draw offset to be centered

--- Map.cpp
+++ Map.cpp
@@ -65,6 +65,91 @@ Map::Map(int level)
 	nodes[5]->neighbours.push_back(nodes[13]);
 	nodes[13]->neighbours.push_back(nodes[5]);
 
+	nodes.push_back(new Node(Coord(12, 9)));
+	nodes[14]->neighbours.push_back(nodes[13]);
+	nodes[13]->neighbours.push_back(nodes[14]);
+
+	nodes[14]->neighbours.push_back(nodes[4]);
+	nodes[4]->neighbours.push_back(nodes[14]);
+
+	nodes.push_back(new Node(Coord(15, 9)));
+	nodes[14]->neighbours.push_back(nodes[15]);
+	nodes[15]->neighbours.push_back(nodes[14]);
+
+	nodes.push_back(new Node(Coord(15, 4)));
+	nodes[16]->neighbours.push_back(nodes[15]);
+	nodes[15]->neighbours.push_back(nodes[16]);
+
+	nodes.push_back(new Node(Coord(21, 4)));
+	nodes[16]->neighbours.push_back(nodes[17]);
+	nodes[17]->neighbours.push_back(nodes[16]);
+
+	nodes.push_back(new Node(Coord(27, 4)));
+	nodes[18]->neighbours.push_back(nodes[17]);
+	nodes[17]->neighbours.push_back(nodes[18]);
+
+	nodes.push_back(new Node(Coord(21, 9)));
+	nodes[17]->neighbours.push_back(nodes[19]);
+	nodes[19]->neighbours.push_back(nodes[17]);
+
+	nodes.push_back(new Node(Coord(27, 9)));
+	nodes[20]->neighbours.push_back(nodes[19]);
+	nodes[19]->neighbours.push_back(nodes[20]);
+
+	nodes[20]->neighbours.push_back(nodes[18]);
+	nodes[18]->neighbours.push_back(nodes[20]);
+
+	nodes.push_back(new Node(Coord(18, 9)));
+	nodes[19]->neighbours.push_back(nodes[21]);
+	nodes[21]->neighbours.push_back(nodes[19]);
+
+	nodes[15]->neighbours.push_back(nodes[21]);
+	nodes[21]->neighbours.push_back(nodes[15]);
+
+	nodes.push_back(new Node(Coord(18, 12)));
+	nodes[22]->neighbours.push_back(nodes[21]);
+	nodes[21]->neighbours.push_back(nodes[22]);
+
+	nodes.push_back(new Node(Coord(15, 12)));
+	nodes[22]->neighbours.push_back(nodes[23]);
+	nodes[23]->neighbours.push_back(nodes[22]);
+
+	nodes.push_back(new Node(Coord(15, 15)));
+	nodes[24]->neighbours.push_back(nodes[23]);
+	nodes[23]->neighbours.push_back(nodes[24]);
+
+	nodes[24]->neighbours.push_back(nodes[10]);
+	nodes[10]->neighbours.push_back(nodes[24]);
+
+	nodes.push_back(new Node(Coord(18, 15)));
+	nodes[24]->neighbours.push_back(nodes[25]);
+	nodes[25]->neighbours.push_back(nodes[24]);
+
+	nodes.push_back(new Node(Coord(18, 18)));
+	nodes[26]->neighbours.push_back(nodes[25]);
+	nodes[25]->neighbours.push_back(nodes[26]);
+
+	nodes.push_back(new Node(Coord(21, 18)));
+	nodes[26]->neighbours.push_back(nodes[27]);
+	nodes[27]->neighbours.push_back(nodes[26]);
+
+	nodes.push_back(new Node(Coord(27, 18)));
+	nodes[28]->neighbours.push_back(nodes[27]);
+	nodes[27]->neighbours.push_back(nodes[28]);
+
+	nodes.push_back(new Node(Coord(21, 12)));
+	nodes[27]->neighbours.push_back(nodes[29]);
+	nodes[29]->neighbours.push_back(nodes[27]);
+
+	nodes[19]->neighbours.push_back(nodes[29]);
+	nodes[29]->neighbours.push_back(nodes[19]);
+
+	nodes.push_back(new Node(Coord(27, 12)));
+	nodes[30]->neighbours.push_back(nodes[29]);
+	nodes[29]->neighbours.push_back(nodes[30]);
+
+	nodes[30]->neighbours.push_back(nodes[20]);
+	nodes[20]->neighbours.push_back(nodes[30]);
 
 	return;
 }
@@ -79,8 +164,8 @@ void Map::draw(Vector2 drawOffset)
 Node* Map::getNodeFromScreenspace(Vector2 screenPosition, Vector2 drawOffset)
 {
 	Coord coordToCheck = Coord(
-		((int)screenPosition.x - drawOffset.x + (GRID_UNIT_SIZE / 2)) / GRID_UNIT_SIZE,
-		((int)screenPosition.y - drawOffset.y + (GRID_UNIT_SIZE / 2)) / GRID_UNIT_SIZE
+		((int)screenPosition.x - drawOffset.x) / GRID_UNIT_SIZE,
+		((int)screenPosition.y - drawOffset.y) / GRID_UNIT_SIZE
 	);
 
 	for (Node* node : nodes)
--- Node.cpp
+++ Node.cpp
@@ -2,32 +2,33 @@
 
 void Node::draw(bool hovered, Vector2 drawOffset)
 {
+	Vector2 tileDrawPosition = Vector2Add(drawOffset, { GRID_UNIT_SIZE / 2, GRID_UNIT_SIZE / 2 });
 	for (Node* neighbour : neighbours)
 	{
 		if (hovered)
 		{
 			DrawLineV(
-				Vector2Add(coordToScreenSpace(coord), drawOffset),
-				Vector2Add(coordToScreenSpace(neighbour->coord), drawOffset),
+				Vector2Add(coordToScreenSpace(coord), tileDrawPosition),
+				Vector2Add(coordToScreenSpace(neighbour->coord), tileDrawPosition),
 				RED
 			);
 		}
 		else
 		{
 			DrawLineV(
-				Vector2Add(coordToScreenSpace(coord), drawOffset),
-				Vector2Add(coordToScreenSpace(neighbour->coord), drawOffset),
+				Vector2Add(coordToScreenSpace(coord), tileDrawPosition),
+				Vector2Add(coordToScreenSpace(neighbour->coord), tileDrawPosition),
 				DARKPURPLE
 			);
 		}
 	}
 	if (hovered)
 	{
-		DrawCircleV(Vector2Add(coordToScreenSpace(coord), drawOffset), 3, BLUE);
+		DrawCircleV(Vector2Add(coordToScreenSpace(coord), tileDrawPosition), 3, BLUE);
 	}
 	else
 	{
-		DrawCircleV(Vector2Add(coordToScreenSpace(coord), drawOffset), 3, GREEN);
+		DrawCircleV(Vector2Add(coordToScreenSpace(coord), tileDrawPosition), 3, GREEN);
 	}
 }
 
